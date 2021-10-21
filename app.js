10e9e40a2ef06376c9f1d21c8054e6c3b7181a71
salomege <salomegelenidze@gmail.com>
2021-10-21 17:43:41 +0400

    JS homework

---
+++ app.js
@@ -0,0 +1,50 @@
+let str='my name is Salome';
+let myAge=31;
+let iAmAdult=true;
+let iAmteenager=false;
+let isNotShown=null;
+let isNotWritten=undefined;
+
+
+console.log (typeof str);
+console.log(typeof myAge);
+console.log (typeof iAmAdult);
+console.log (typeof iAmteenager);
+console.log (typeof isNotShown);
+console.log (typeof isNotWritten);
+
+
+
+let add = 100 + 500;
+let sub = 1000 - 500;
+let multiple = 1000 * 500;
+let div = 1000 / 500;
+
+
+console.log('add - ',add);
+console.log( 'sub - ', sub);
+console.log('multiple - ', multiple);
+console.log('div - ', div);
+
+var a = 10;
+var b = 10;
+console.log(`the sum of a and b ${a+b}`);
+
+
+let length = 7;
+let width = 5;
+
+console.log (length * width);
+
+let side1 = 5; 
+let side2 = 6; 
+let side3 = 6; 
+let s = (side1 + side2 + side3)/2;
+let area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+console.log(area);
+
+
+let radius = 7;
+console.log("The Area =", Math.PI * Math.pow('radius', 2));
+
+
